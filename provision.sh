b1536c497d004a4d2c9f87292b9f5ad98835f174
David Bishop <david@gnuconsulting.com>
2015-01-11 22:13:05 -0500

    add the cf client at the end of the provision script

--- provision.sh
+++ provision.sh
@@ -125,3 +125,8 @@ done
 
 # FIXME: enable this again when smoke_tests work
 # bosh run errand smoke_tests
+
+# Install the cf client for use on the bastion server
+wget -O /tmp/cf.tgz 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github'
+tar -zxvf /tmp/cf.tgz
+mv /tmp/cf /usr/local/bin/
