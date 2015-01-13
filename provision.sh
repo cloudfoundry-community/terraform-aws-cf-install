51888a4689cb746926d3c264f30b32a1064d3f2a
David Bishop <david@gnuconsulting.com>
2015-01-12 23:58:52 -0500

    final fix for the bosh deploy in provision.sh

--- provision.sh
+++ provision.sh
@@ -114,8 +114,8 @@ bosh prepare deployment
 
 # Speaking of hack-work, bosh deploy often fails the first or even second time, due to packet bats
 # We run it up to three times (it's idempotent) so that you don't have to
-counter=1
-while [ $x -lt 3 ]; do
+counter=0
+while [ $counter -lt 3 ]; do
   bosh -n deploy
   if [ $? -eq 0 ]; then
     break
