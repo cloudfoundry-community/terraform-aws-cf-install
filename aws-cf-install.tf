453cf4a2144903ea08f2c9261fc108bb4112727d
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-09 16:58:54 -0500

    removed lb because its no longer used

--- aws-cf-install.tf
+++ aws-cf-install.tf
@@ -58,7 +58,6 @@ module "cf" {
   aws_internet_gateway_id = "${module.vpc.aws_internet_gateway_id}"
   aws_route_table_public_id = "${module.vpc.aws_route_table_public_id}"
   aws_route_table_private_id = "${module.vpc.aws_route_table_private_id}"
-  aws_subnet_lb_availability_zone = "${module.vpc.aws_subnet_bastion_availability_zone}"
   aws_subnet_cfruntime-2a_availability_zone = "${var.cf1_az}"
   aws_subnet_cfruntime-2b_availability_zone = "${var.cf2_az}"
 }
