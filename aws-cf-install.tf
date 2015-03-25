ce04a79ea61131445e74b29d2f1096376c1b26de
David Bishop <david@gnuconsulting.com>
2015-03-25 11:27:31 -0400

    add first run at automating installing docker services

--- aws-cf-install.tf
+++ aws-cf-install.tf
@@ -66,6 +66,10 @@ output "cf_api" {
 	value = "api.run.${module.cf.aws_eip_cf_public_ip}.xip.io"
 }
 
+output "aws_subnet_docker_id" {
+  value = "module.cf.aws_subnet_docker_id"
+}
+
 resource "aws_instance" "bastion" {
   ami = "${lookup(var.aws_ubuntu_ami, var.aws_region)}"
   instance_type = "m1.medium"
@@ -95,7 +99,7 @@ resource "aws_instance" "bastion" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /home/ubuntu/provision.sh",
-        "/home/ubuntu/provision.sh ${var.aws_access_key} ${var.aws_secret_key} ${var.aws_region} ${module.vpc.aws_vpc_id} ${module.vpc.aws_subnet_microbosh_id} ${var.network} ${module.cf.aws_eip_cf_public_ip} ${module.cf.aws_subnet_cfruntime-2a_id} ${module.cf.aws_subnet_cfruntime-2a_availability_zone} ${module.cf.aws_subnet_cfruntime-2b_id} ${module.cf.aws_subnet_cfruntime-2b_availability_zone} ${aws_instance.bastion.availability_zone} ${aws_instance.bastion.id} ${module.cf.aws_subnet_lb_id} ${module.cf.aws_security_group_cf_name} ${var.cf_admin_pass} ${var.cf_domain} ${var.cf_boshworkspace_version} ${var.cf_size}",
+        "/home/ubuntu/provision.sh ${var.aws_access_key} ${var.aws_secret_key} ${var.aws_region} ${module.vpc.aws_vpc_id} ${module.vpc.aws_subnet_microbosh_id} ${var.network} ${module.cf.aws_eip_cf_public_ip} ${module.cf.aws_subnet_cfruntime-2a_id} ${module.cf.aws_subnet_cfruntime-2a_availability_zone} ${module.cf.aws_subnet_cfruntime-2b_id} ${module.cf.aws_subnet_cfruntime-2b_availability_zone} ${aws_instance.bastion.availability_zone} ${aws_instance.bastion.id} ${module.cf.aws_subnet_lb_id} ${module.cf.aws_security_group_cf_name} ${var.cf_admin_pass} ${var.cf_domain} ${var.cf_boshworkspace_version} ${var.cf_size} ${module.cf.aws_subnet_docker_id} ${var.install_docker_services}",
     ]
   }
 
